494a0515461264adb0bf4b24db15a99a7c99ae87
samuelowino <samuelowino43@yahoo.com>
2019-12-31 00:35:09 +0300

    start spring io

---
+++ src/main/java/com/jpaguide/code/JpaCodeLabApplication.java
@@ -0,0 +1,13 @@
+package com.jpaguide.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaCodeLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaCodeLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpaguide/code/JpaCodeLabApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jpaguide.code;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JpaCodeLabApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
